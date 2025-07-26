232e337da6e35b7df01fb1d5c11e7760e9614537
Anthony Sarkis <18080164+anthony-chaudhary@users.noreply.github.com>
2025-07-26 10:52:42 -0700

    config

--- v2/frontend/src/netra-frontend-v2/next.config.ts
+++ v2/frontend/src/netra-frontend-v2/next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/v2/:path*',
+        destination: 'http://127.0.0.1:8000/api/v2/:path*', // Proxy to Backend
+      },
+      {
+        source: '/token',
+        destination: 'http://127.0.0.1:8000/token', // Proxy to Backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
