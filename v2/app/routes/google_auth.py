2034900a5f5df716fef0befa322e748d18da8911
Anthony Sarkis <18080164+anthony-chaudhary@users.noreply.github.com>
2025-08-04 15:15:10 -0700

    Update google_auth.py

--- v2/app/routes/google_auth.py
+++ v2/app/routes/google_auth.py
@@ -16,8 +16,8 @@ oauth = OAuth()
 oauth.register(
     name='google',
     server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-    client_id=settings.google_model.client_id,
-    client_secret=settings.google_model.client_secret,
+    client_id=settings.google_cloud.client_id,
+    client_secret=settings.google_cloud.client_secret,
     client_kwargs={
         'scope': 'openid email profile'
     }
