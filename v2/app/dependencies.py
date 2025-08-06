f1e9e69135366321b29fac3583da9a9b7dbf215e
Anthony Sarkis <18080164+anthony-chaudhary@users.noreply.github.com>
2025-08-05 21:14:48 -0700

    Update dependencies.py

--- v2/app/dependencies.py
+++ v2/app/dependencies.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import Depends, HTTPException, status, Request
+from fastapi import Depends, HTTPException, status, Request, WebSocket, Query
 from sqlmodel import Session
 
 from app.db.postgres import get_async_db
@@ -8,6 +8,7 @@ from app import schema
 from app.services.security_service import oauth2_scheme
 from app.llm.llm_manager import LLMManager
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.config import settings
 
 DbDep = Annotated[Session, Depends(get_async_db)]
 TokenDep = Annotated[str, Depends(oauth2_scheme)]
@@ -39,5 +40,30 @@ def get_current_user(
         )
     return user
 
+async def get_current_user_ws(
+    websocket: WebSocket,
+    token: str = Query(None),
+) -> models_postgres.User:
+    if settings.app_env == "development":
+        return models_postgres.User(email="dev@example.com", hashed_password="dev")
+
+    if token is None:
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
+        return
+
+    security_service = websocket.app.state.security_service
+    email = security_service.get_user_email_from_token(token)
+    if email is None:
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
+        return
+
+    async with websocket.app.state.db_session_factory() as session:
+        user = await security_service.get_user(session, email)
+        if user is None:
+            await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
+            return
+        return user
+
 ActiveUserDep = Annotated[models_postgres.User, Depends(get_current_user)]
 LLMManagerDep = Annotated[LLMManager, Depends(get_llm_manager)]
+ActiveUserWsDep = Annotated[models_postgres.User, Depends(get_current_user_ws)]
