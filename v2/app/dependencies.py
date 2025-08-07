b03ce5ed44412c6fd25ce7bf78db4984f4362158
Anthony Sarkis <18080164+anthony-chaudhary@users.noreply.github.com>
2025-08-07 14:16:49 -0700

    a

--- v2/app/dependencies.py
+++ v2/app/dependencies.py
@@ -5,13 +5,11 @@ from sqlmodel import Session
 from app.db.postgres import get_async_db
 from app.db import models_postgres
 from app import schemas
-from app.services.security_service import oauth2_scheme
 from app.llm.llm_manager import LLMManager
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.config import settings
 
 DbDep = Annotated[Session, Depends(get_async_db)]
-TokenDep = Annotated[str, Depends(oauth2_scheme)]
 
 def get_llm_manager(request: Request) -> LLMManager:
     return request.app.state.llm_manager
@@ -21,16 +19,23 @@ async def get_db_session(request: Request) -> AsyncSession:
         yield session
 
 async def get_current_user(
-    token: TokenDep, db: DbDep, request: Request
+    db: DbDep, request: Request
 ) -> models_postgres.User:
-    security_service = request.app.state.security_service
-    email = security_service.get_user_email_from_token(token)
-    if email is None:
+    user_info = request.session.get('user')
+    if not user_info:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Could not validate credentials",
             headers={"WWW-Authenticate": "Bearer"},
         )
+    email = user_info.get('email')
+    if not email:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    security_service = request.app.state.security_service
     user = await security_service.get_user(db, email)
     if user is None:
         raise HTTPException(
