5a562c18689a0abc02058574aec71853070e7072
Anthony Sarkis <18080164+anthony-chaudhary@users.noreply.github.com>
2025-08-08 09:55:53 -0700

    a

--- v2/frontend/cypress.config.ts
+++ v2/frontend/cypress.config.ts
@@ -1,10 +1,12 @@
+
 import { defineConfig } from 'cypress';
+import { API_BASE_URL } from './services/apiConfig';
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: API_BASE_URL.replace('8000', '3000'),
   },
 });
