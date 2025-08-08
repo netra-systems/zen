5350fbafe874fa35c8e88fdc243c2335a29a58d2
Anthony Sarkis <18080164+anthony-chaudhary@users.noreply.github.com>
2025-08-08 10:02:48 -0700

    a

--- v2/frontend/app/config.ts
+++ v2/frontend/app/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   api: {
-    baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000',
-    wsBaseUrl: process.env.NEXT_PUBLIC_WEBSOCKET_URL || 'ws://localhost:8000',
+    baseUrl:  'http://localhost:8000',
+    wsBaseUrl: 'ws://localhost:8000',
   },
 };
