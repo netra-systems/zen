4ff1342bf115c6d4dfa8cf8e587f7b9650f505a3
Anthony Sarkis <18080164+anthony-chaudhary@users.noreply.github.com>
2025-08-08 11:34:22 -0700

    a

--- v2/frontend/components/AppWithLayout.tsx
+++ v2/frontend/components/AppWithLayout.tsx
@@ -1,26 +1,22 @@
 'use client';
 
-import { useState } from 'react';
 import { Sidebar } from '@/components/Sidebar';
 import { Header } from '@/components/Header';
 import { cn } from '@/lib/utils';
 import { AppWithLayoutProps } from '@/types';
+import useAppStore from '@/store';
 
 export function AppWithLayout({ children }: AppWithLayoutProps) {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
+  const { isSidebarCollapsed, toggleSidebar } = useAppStore();
 
   return (
     <div
       className={cn(
         'grid min-h-screen w-full',
-        isSidebarOpen && 'md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]'
+        !isSidebarCollapsed && 'md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]'
       )}
     >
-      {isSidebarOpen && <Sidebar />}
+      {!isSidebarCollapsed && <Sidebar />}
       <div className="flex flex-col">
         <Header toggleSidebar={toggleSidebar} />
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
