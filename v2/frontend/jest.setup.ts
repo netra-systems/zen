8529189e5fbd183050ffa032ffe3c031469f5fe6
Anthony Sarkis <18080164+anthony-chaudhary@users.noreply.github.com>
2025-08-07 22:46:17 -0700

    a

--- v2/frontend/jest.setup.ts
+++ v2/frontend/jest.setup.ts
@@ -1,10 +1,1 @@
 import '@testing-library/jest-dom';
-import 'jest-websocket-mock';
-
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () => Promise.resolve({}),
-  })
-) as jest.Mock;
-
-window.HTMLElement.prototype.scrollIntoView = jest.fn();
