39422e70c6904af31f391e10307da58ed33b091e
Anthony Sarkis <18080164+anthony-chaudhary@users.noreply.github.com>
2025-08-08 20:41:05 -0700

    Update next.config.ts

--- v2/frontend/next.config.ts
+++ v2/frontend/next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/:path*', // Proxy to Backend
+        destination: 'http://127.0.0.1:8000/api/:path*', // Proxy to Backend
       },
       {
         source: '/token',
